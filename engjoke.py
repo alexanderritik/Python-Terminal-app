1e929f16c9f122dbaf37c42fad6ddac82b288bb6
Ritik Srivastava <rakeshkusrivastav@gmail.com>
2020-07-17 06:06:26 +0530

    Update engjoke.py

--- engjoke.py
+++ engjoke.py
@@ -3,7 +3,7 @@ import requests
 response = requests.get("https://joke3.p.rapidapi.com/v1/joke",
   headers={
     "X-RapidAPI-Host": "joke3.p.rapidapi.com",
-    "X-RapidAPI-Key": "83a77d59f4msh721e0959e3a7c66p12844bjsn3a783262714d",
+    "X-RapidAPI-Key": " ",
     "Content-Type": "application/json"
   },
   params=("{\"content\":\"A joke here\",\"nsfw\":\"false\"}")
